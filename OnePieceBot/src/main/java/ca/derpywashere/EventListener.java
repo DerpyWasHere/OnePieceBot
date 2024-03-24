ee04497dd79a5cc54c1b2bfda7a395e295836ed5
DerpyWasHere <smc26@sfu.ca>
2024-03-24 08:27:23 -0700

    OnePieceBot: Initialized Maven project

---
+++ OnePieceBot/src/main/java/ca/derpywashere/EventListener.java
@@ -0,0 +1,21 @@
+package ca.derpywashere;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class EventListener extends ListenerAdapter
+{
+    public EventListener(JDA jda)
+    {
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+
+    }
+}
