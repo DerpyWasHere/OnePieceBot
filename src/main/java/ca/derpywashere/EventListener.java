561053f6b0028e83f9f36ee7e4d20d84f33c5eae
DerpyWasHere <smc26@sfu.ca>
2024-03-24 10:03:44 -0700

    OnePieceBot: Added "one" and "piece" text recognition functionality

--- src/main/java/ca/derpywashere/EventListener.java
+++ src/main/java/ca/derpywashere/EventListener.java
@@ -6,16 +6,24 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+/**
+ * Override of JDA EventListener
+ * Listens for messages sent in Discord guilds
+ *
+ * @author DerpyWasHere
+ */
 public class EventListener extends ListenerAdapter
 {
-    public EventListener(JDA jda)
-    {
-
-    }
+    public EventListener(JDA jda) {}
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        String str;
+        String str = event.getMessage().getContentRaw();
+        String lowercase_str = str.toLowerCase();
+        if(lowercase_str.contains("one") || lowercase_str.contains("piece"))
+        {
+            event.getChannel().sendMessage("https://derpywashere.s-ul.eu/KhHYXcyo").queue();
+        }
     }
 }
