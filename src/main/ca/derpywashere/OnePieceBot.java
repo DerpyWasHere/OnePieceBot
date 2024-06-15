991c0736c46ea55eca36634f497cf827bcc6b192
DerpyWasHere <seanchan@telus.net>
2024-06-15 02:33:53 -0700

    OnePieceBot: Changed parameters for runtime to include database credentials, changed parameters in EventListener constructor

--- src/main/ca/derpywashere/OnePieceBot.java
+++ src/main/ca/derpywashere/OnePieceBot.java
@@ -11,17 +11,17 @@ public class OnePieceBot
 {
     public static void main(String[] args)
     {
-        init(args[0]);
+        init(args[0], args[1], args[2], args[3]);
     }
 
-    public static void init(String key)
+    public static void init(String key, String db_user, String db_password, String db_ip)
     {
         try
         {
             JDABuilder builder = JDABuilder.createDefault(key);
             builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
             JDA jda = builder.build();
-            jda.addEventListener(new EventListener());
+            jda.addEventListener(new EventListener(db_user, db_password, db_ip));
             jda.awaitReady();
         }
         catch(InterruptedException interruptedEx)
